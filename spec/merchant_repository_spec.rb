8128e07891cd76f0cdcf114f0b82d5b61dcc66b6
Sarah <sarahrudy@gmail.com>
2021-04-12 16:01:31 -0600

    Instantiate new objects

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -1,1 +1,19 @@
-require 
+require './spec/spec_helper'
+
+RSpec.describe MerchantRepository do
+  @sale_engine = SalesEngine.new
+  describe 'instantiation' do
+    it "::new" do
+      merchant_repository = MerchantRepository.new
+
+      expect(merchant_repository).to be_instance_of(MerchantRepository)
+    end
+  end
+
+  it 'can return an array of all known merchant instances'do
+      merchant_repository = @sales_engine.merchant
+      merchant =
+
+      #
+
+end
