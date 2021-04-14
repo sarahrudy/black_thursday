006aba92dca798e8f32fd5eb3d72f1c2b676f524
Sarah <sarahrudy@gmail.com>
2021-04-13 18:57:48 -0600

    Initialize class and add methods

--- lib/item.rb
+++ lib/item.rb
@@ -1,10 +1,18 @@
+require 'bigdecimal'
 class Item
+  attr_reader :id,
+              :name,
+              :description,
+              :unit_price,
+              :created_at,
+              :updated_at,
+              :merchant_id
   def initialize(id:, name:, description:, unit_price:, created_at:, updated_at:, merchant_id:)
     @id = id
     @name = name
     @description = description
-    @unit_price = unit_price
-    @created_at = DateTime.parse(created_at)
+    @unit_price = BigDecimal(unit_price)
+    @created_at = DateTime.parse(created_at) # parse takes a string so we have to give it a string
     @updated_at = DateTime.parse(updated_at)
     @merchant_id = merchant_id
   end
