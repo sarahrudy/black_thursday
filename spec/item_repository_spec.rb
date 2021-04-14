c997b8507b036341cba93dbe95518c3cb99f1727
Sarah <sarahrudy@gmail.com>
2021-04-13 19:04:58 -0600

    Instantiate new objects and add tests

---
+++ spec/item_repository_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.describe ItemRepository do
+  before(:each) do
+    @sales_engine = SalesEngine.new
+    @sales_engine.from_csv({
+                            items: './data/items.csv'
+                         })
+  end
+  describe 'instantiation' do
+    it "::new" do
+      item_repository = ItemRepository.new([])
+
+      expect(item_repository).to be_instance_of(ItemRepository)
+    end
+  end
+
+  describe 'item repository methods' do
+    it 'can return an array of all known item instances'do
+      item_repository = @sales_engine.items
+
+      # look into a different way of wording our test.
+      expect(item_repository.all).to eq(item_repository.items)
+    end
+
+    it 'create item' do
+      item_repository = @sales_engine.items
+      item_1 = item_repository.create({
+                                      :name        => "iPhone Case",
+                                      :description => "Can drop phone without breaking it",
+                                      :unit_price  => BigDecimal(10.99,4),
+                                      :created_at  => DateTime.now.to_s,
+                                      :updated_at  => DateTime.now.to_s,
+                                      :merchant_id => 2
+                                    })
+
+      expect(item_1).to be_instance_of(Item)
+    end
+  end
+
+end
