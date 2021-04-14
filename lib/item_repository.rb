6ea0c4046fa38a122fa20d32220a52f71a430aa5
Sarah <sarahrudy@gmail.com>
2021-04-13 18:58:47 -0600

    Add to_i to 'create' method

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,5 +1,15 @@
-class ItemsRepository
+require './lib/repository'
+class ItemRepository < Repository
+  attr_reader :items
   def initialize(items)
+    super(items)
     @items = items
+  end
+
+  def create(attributes)
+    attributes[:id] = find_last_id.to_i + 1
+    item = Item.new(attributes)
+    @items << item
+    item  # returning instance of the item
   end
 end
