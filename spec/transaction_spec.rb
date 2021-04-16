16b696e2d399e8892c81664f618596f75f141bc4
MarkY <markcyen@yahoo.com>
2021-04-15 21:17:28 -0600

    Fix credit card info to be string

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe Transaction do
         transaction = Transaction.new({
                         :id                           => 1234567,
                         :invoice_id                   => 8901011,
-                        :credit_card_number           => 1234567890123456,
-                        :credit_card_expiration_date  => 1221,
+                        :credit_card_number           => '1234567890123456',
+                        :credit_card_expiration_date  => '1221',
                         :result                       => 'success',
                         :created_at                   => Time.now.to_s,
                         :updated_at                   => Time.now.to_s
