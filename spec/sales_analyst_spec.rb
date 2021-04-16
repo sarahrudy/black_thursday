8166479b76522962bf07d0bf2156c4e21710679d
Zach Green <zachjamesgreen@gmail.com>
2021-04-15 20:10:56 -0600

    Add test and method for average item price for merchant

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe SalesAnalyst do
   end
 
    describe 'instance methods' do
-    it '#average_items_per_merchant' do
+     it '#average_items_per_merchant' do
      sales_analyst = @sales_engine.analyst
 
      expect(sales_analyst.average_items_per_merchant).to eq(2.88)
@@ -32,6 +32,25 @@ RSpec.describe SalesAnalyst do
      sales_analyst = @sales_engine.analyst
 
      expect(sales_analyst.merchants_with_high_item_count.size).to eq(49)
-   end
+    end
+    it '#average_item_price_for_merchant' do
+
+      sales_analyst = @sales_engine.analyst
+      merchant = @sales_engine.merchants.create(attributes_for(:merchant))
+      item_repo = @sales_engine.items
+      price = BigDecimal(0)
+      
+      4.times do |i|
+        attrs = attributes_for(:item)
+        attrs[:merchant_id] = merchant.id
+        # require "pry"; binding.pry
+        item = item_repo.create(attrs)
+        merchant.add_item(item)
+        price += item.unit_price
+      end
+      average_price = price/4
+      
+      expect(sales_analyst.average_item_price_for_merchant(merchant.id)).to eq(average_price)
+    end
   end
 end
