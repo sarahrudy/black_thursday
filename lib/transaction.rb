db70592384b6055d674e6a98d6e563ae97ca9d3b
MarkY <markcyen@yahoo.com>
2021-04-15 21:14:15 -0600

    Implement initialize method

---
+++ lib/transaction.rb
@@ -0,0 +1,21 @@
+require 'time'
+
+class Transaction
+  attr_reader   :id,
+                :invoice_id,
+                :created_at
+  attr_accessor :credit_card_number,
+                :credit_card_expiration_date,
+                :result,
+                :updated_at
+
+  def initialize(id:, invoice_id:, created_at:, credit_card_number:, credit_card_expiration_date:, result:, updated_at:)
+    @id = id.to_i
+    @invoce_id = invoice_id.to_i
+    @created_at = Time.parse(created_at.to_s)
+    @credit_card_number = credit_card_number.to_i
+    @credit_card_expiration_date = credit_card_expiration_date.to_i
+    @result = result
+    @updated_at = Time.parse(updated_at.to_s)
+  end
+end
