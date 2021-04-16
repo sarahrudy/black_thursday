b90a7fae470ea1e00dc3a9451717e9226d17e503
MarkY <markcyen@yahoo.com>
2021-04-15 23:08:22 -0600

    Create initialize test

---
+++ spec/transaction_repository_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe TransactionRepository do
+  before(:each) do
+    @sales_engine = SalesEngine.from_csv({
+                            items: './data/items.csv',
+                            merchants: './data/merchants.csv',
+                            invoices: './data/invoices.csv',
+                            transactions: './data/transactions.csv'
+                         })
+  end
+  describe 'instantiation' do
+    it "::new" do
+      transaction_repository = @sales_engine.transactions
+
+      expect(transaction_repository).to be_instance_of(TransactionRepository)
+    end
+  end
+
+  # describe 'transaction repository methods' do
+  #   it 'can return an array of all known transaction instances'do
+  #     transaction_repository = @sales_engine.transactions
+  #
+  #     # look into a different way of wording our test.
+  #     expect(transaction_repository.all).to eq(transaction_repository.transactions)
+  #   end
+  #
+  # end
+end
