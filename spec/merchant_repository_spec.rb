0cffd1d53b502a32f21bd3a017075231f3b2dc2f
MarkY <markcyen@yahoo.com>
2021-04-15 18:39:02 -0600

    Add invoices path to csv file

--- spec/merchant_repository_spec.rb
+++ spec/merchant_repository_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe MerchantRepository do
     @sales_engine = SalesEngine.from_csv({
                             items: './data/items.csv',
                             merchants: './data/merchants.csv',
+                            invoices: './data/invoices.csv'
                          })
   end
   describe 'instantiation' do
