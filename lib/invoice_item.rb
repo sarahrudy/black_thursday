bda6b2023fc81a796213100ba703f5513434feaa
MarkY <markcyen@yahoo.com>
2021-04-19 17:55:36 -0600

    Convert quantity to integer

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -14,11 +14,11 @@ class InvoiceItem
     @item_id = item_id.to_i
     @invoice_id = invoice_id.to_i
     @created_at = Time.parse(created_at.to_s)
-    @quantity = quantity
+    @quantity = quantity.to_i
     @unit_price = BigDecimal((unit_price.to_i / 100.to_f), 4)
     @updated_at = Time.parse(updated_at.to_s)
   end
-  
+
   def unit_price_to_dollars
     @unit_price.to_f
   end
