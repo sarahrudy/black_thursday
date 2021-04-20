d75faf181e982b616e7cf4e62c969b0da86430b2
Zach Green <zachjamesgreen@gmail.com>
2021-04-20 10:37:04 -0600

    WIP changes to tests

--- spec/sales_analyst_spec.rb
+++ spec/sales_analyst_spec.rb
@@ -1,20 +1,9 @@
 require_relative 'spec_helper'
 
 RSpec.describe SalesAnalyst do
-  before(:each) do
-    @sales_engine = SalesEngine.from_csv({
-
-                            items: './data/items.csv',
-                            merchants: './data/merchants.csv',
-                            invoices: './data/invoices.csv',
-                            invoice_items: './data/invoice_items.csv',
-                            transactions: './data/transactions.csv',
-                            customers: './data/customers.csv',
-                                         })
-  end
+  let(:sales_analyst) { engine.analyst }
   describe 'instantiation' do
     it '::new' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst).to be_instance_of(SalesAnalyst)
     end
@@ -22,27 +11,23 @@ RSpec.describe SalesAnalyst do
 
   describe 'instance methods' do
     it '#average_items_per_merchant' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.average_items_per_merchant).to eq(2.88)
     end
 
     it '#average_items_per_merchant_standard_deviation' do
-      sales_analyst = @sales_engine.analyst
       # come back to make sure it's ok to use 3.16
       expect(sales_analyst.average_items_per_merchant_standard_deviation).to eq(3.26)
     end
 
     it '#merchants_with_high_item_count' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.merchants_with_high_item_count.size).to eq(52)
     end
 
     it '#average_item_price_for_merchant' do
-      sales_analyst = @sales_engine.analyst
-      merchant = @sales_engine.merchants.create({ name: 'Zachs Store' })
-      item_repo = @sales_engine.items
+      merchant = engine.merchants.create({ name: 'Zachs Store' })
+      item_repo = engine.items
       price = BigDecimal(0)
       item1 = item_repo.create({ name: 'item1', description: 'Description1', unit_price: 12_345, created_at: Time.now,
                                  updated_at: Time.now, merchant_id: merchant.id })
@@ -58,51 +43,18 @@ RSpec.describe SalesAnalyst do
         merchant.add_item(item)
         price += item.unit_price
       end
-      average_price = BigDecimal(price / 4, 5)
+      average_price = BigDecimal((price / 4), 5)
+      # require "pry"; binding.pry
 
       expect(sales_analyst.average_item_price_for_merchant(merchant.id)).to eq(average_price.round(2))
     end
-
+    
     it '#average_average_price_per_merchant' do
-      sales_analyst = @sales_engine.analyst
-      item_repo = @sales_engine.items
-      @sales_engine.merchants.all.clear
-      merchant1 = @sales_engine.merchants.create({ name: 'Zach Shop1' })
-      merchant2 = @sales_engine.merchants.create({ name: 'Zach Shop2' })
-      merchant3 = @sales_engine.merchants.create({ name: 'Zach Shop3' })
-      merchants = [merchant1, merchant2, merchant3]
 
-      prices = {
-        merchant1 => [],
-        merchant2 => [],
-        merchant3 => []
-      }
-
-      merchants.each do |merchant|
-        4.times do |i|
-          item = item_repo.create({ name: "item#{i}", description: "Description#{i}", unit_price: rand(100..99_999),
-                                    created_at: Time.now, updated_at: Time.now, merchant_id: merchant.id })
-          merchant.add_item(item)
-          prices[merchant] << item.unit_price
-        end
-      end
-
-      average_prices = prices.each do |k, v|
-        prices[k] = v.sum / v.size
-      end
-
-      average = []
-      average_prices.each do |_, v|
-        average << v
-      end
-
-      average = BigDecimal((average.sum / average.size), 5).round(2)
-
-      expect(sales_analyst.average_average_price_per_merchant).to eq(average)
+      expect(sales_analyst.average_average_price_per_merchant).to eq(349.64)
     end
 
     it '#golden_items' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.golden_items.size).to eq(5)
     end
@@ -110,37 +62,31 @@ RSpec.describe SalesAnalyst do
 
   describe 'business intelligence on invoices' do
     it '#average_invoices_per_merchant' do
-      sales_analyst = @sales_engine.analyst
 
-      expect(sales_analyst.average_invoices_per_merchant).to eq(10.49)
+      expect(sales_analyst.average_invoices_per_merchant).to eq(10.47)
     end
 
     it '#average_invoices_per_merchant_standard_deviation' do
-      sales_analyst = @sales_engine.analyst
       # come back to make sure it's ok to use 3.16
-      expect(sales_analyst.average_invoices_per_merchant_standard_deviation).to eq(3.29)
+      expect(sales_analyst.average_invoices_per_merchant_standard_deviation).to eq(3.32)
     end
 
     it '#top_merchants_by_invoice_count' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.top_merchants_by_invoice_count.size).to eq(12)
     end
 
     it '#bottom_merchants_by_invoice_count' do
-      sales_analyst = @sales_engine.analyst
 
-      expect(sales_analyst.bottom_merchants_by_invoice_count.size).to eq(4)
+      expect(sales_analyst.bottom_merchants_by_invoice_count.size).to eq(5)
     end
 
     it '#top_days_by_invoice_count' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.top_days_by_invoice_count).to eq(['Wednesday'])
     end
 
     it '#invoice_status' do
-      sales_analyst = @sales_engine.analyst
 
       expect(sales_analyst.invoice_status(:pending)).to eq(29.55)
       expect(sales_analyst.invoice_status(:shipped)).to eq(56.95)
