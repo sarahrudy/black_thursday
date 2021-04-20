77d60ff95efa160810a1b26808ffbdffe53c41d0
Zach Green <zachjamesgreen@gmail.com>
2021-04-20 09:52:21 -0600

    WIP changing how test runs

--- spec/item_repository_spec.rb
+++ spec/item_repository_spec.rb
@@ -1,19 +1,9 @@
 require_relative 'spec_helper'
 
 RSpec.describe ItemRepository do
-  before(:each) do
-    @sales_engine = SalesEngine.from_csv({
-                            items: './data/items.csv',
-                            merchants: './data/merchants.csv',
-                            invoices: './data/invoices.csv',
-                            invoice_items: './data/invoice_items.csv',
-                            transactions: './data/transactions.csv',
-                            customers: './data/customers.csv',
-                         })
-  end
   describe 'instantiation' do
     it '::new' do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
 
       expect(item_repository).to be_instance_of(ItemRepository)
     end
@@ -21,18 +11,20 @@ RSpec.describe ItemRepository do
 
   describe 'item repository methods' do
     it 'can return an array of all known item instances'do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
 
       # look into a different way of wording our test.
+      expect(item_repository.all).to be_instance_of(Array)
+      expect(item_repository.all.first).to be_instance_of(Item)
       expect(item_repository.all).to eq(item_repository.items)
     end
 
     it 'create item' do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
       item_1 = item_repository.create({
                                         :name => 'iPhone Case',
                                         :description => 'Can drop phone without breaking it',
-                                        :unit_price => BigDecimal(10.99, 4),
+                                        :unit_price => BigDecimal(1099, 4),
                                         :created_at => Time.now.to_s,
                                         :updated_at => Time.now.to_s,
                                         :merchant_id => 2
@@ -42,22 +34,22 @@ RSpec.describe ItemRepository do
     end
 
     it 'finds all items with descriptions' do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
       item_1 = item_repository.create({
                                         :name => 'iPhone Case',
-                                        :description => 'Can drop phone without breaking it',
-                                        :unit_price => BigDecimal(10.99, 4),
+                                        :description => 'drop phone without breaking it',
+                                        :unit_price => BigDecimal(1099, 4),
                                         :created_at => Time.now.to_s,
                                         :updated_at => Time.now.to_s,
                                         :merchant_id => 2
                                       })
-
-      expect(item_repository.find_all_with_description(item_1.description)).to eq([item_1])
-      expect(item_repository.find_all_with_description('Phone without Breaking')).to eq([item_1])
+      item_2 = item_repository.find_by_id(263567475)
+      
+      expect(item_repository.find_all_with_description('Phone without Breaking')).to eq([item_2, item_1])
     end
 
     it 'finds all items by price' do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
       item_1 = item_repository.create({
                                         :name => 'iPhone Case',
                                         :description => 'Can drop phone without breaking it',
@@ -67,11 +59,12 @@ RSpec.describe ItemRepository do
                                         :merchant_id => 2
                                       })
 
-      expect(item_repository.find_all_by_price(1099)).to eq([item_1])
+      expect(item_repository.find_all_by_price(1099)).to include(item_1)
+      expect(item_repository.find_all_by_price(1099).size).to eq(3)
     end
 
     it 'finds all items by price in range' do
-      item_repository = @sales_engine.items
+      item_repository = engine.items
       item_1 = item_repository.create({
                                         :name => 'iPhone Case',
                                         :description => 'Can drop phone without breaking it',
@@ -89,34 +82,24 @@ RSpec.describe ItemRepository do
                                         :merchant_id => 2
                                       })
 
-      expect(item_repository.find_all_by_price_in_range(10.00..11.00).size).to eq(67)
+      expect(item_repository.find_all_by_price_in_range(10.00..11.00).size).to eq(70)
       expect(item_repository.find_all_by_price_in_range(10.00..11.00)).to include(item_1)
     end
 
     it 'finds all items by merchant id' do
-      item_repository = @sales_engine.items
-      item_1 = item_repository.create({
-                                        :name => 'iPhone Case',
-                                        :description => 'Can drop phone without breaking it',
-                                        :unit_price => BigDecimal(10.99, 4),
-                                        :created_at => Time.now.to_s,
-                                        :updated_at => Time.now.to_s,
-                                        :merchant_id => 2
-                                      })
+      item_repository = engine.items
+      item_1 = item_repository.find_by_id(263567475)
+      item_2 = item_repository.find_by_id(263567476)
+      item_3 = item_repository.find_by_id(263567477)
+      item_4 = item_repository.find_by_id(263567478)
+      item_5 = item_repository.find_by_id(263567479)
 
-      expect(item_repository.find_all_by_merchant_id(item_1.merchant_id)).to eq([item_1])
+      expect(item_repository.find_all_by_merchant_id(item_1.merchant_id)).to eq([item_1,item_2,item_3,item_4,item_5])
     end
 
     it 'update a specific item' do
-      item_repository = @sales_engine.items
-      item_1 = item_repository.create({
-                                        :name => 'iPhone Case',
-                                        :description => 'Can drop phone without breaking it',
-                                        :unit_price => BigDecimal(10.99, 4),
-                                        :created_at => Time.now.to_s,
-                                        :updated_at => Time.now.to_s,
-                                        :merchant_id => 2
-                                      })
+      item_repository = engine.items
+      item_1 = item_repository.find_by_id(263567475)
 
       attributes = { name: 'iPhone Protective Holder' }
       expected = item_repository.update(item_1.id, attributes)
@@ -125,23 +108,8 @@ RSpec.describe ItemRepository do
     end
 
     it 'can delete item' do
-      item_repository = @sales_engine.items
-      item_1 = item_repository.create({
-                                        :name => 'iPhone Case',
-                                        :description => 'Can drop phone without breaking it',
-                                        :unit_price => 1099,
-                                        :created_at => Time.now.to_s,
-                                        :updated_at => Time.now.to_s,
-                                        :merchant_id => 2
-                                      })
-      item_2 = item_repository.create({
-                                        :name => 'iPhone Case',
-                                        :description => 'Can drop phone without breaking it',
-                                        :unit_price => 1599,
-                                        :created_at => Time.now.to_s,
-                                        :updated_at => Time.now.to_s,
-                                        :merchant_id => 2
-                                      })
+      item_repository = engine.items
+      item_1 = item_repository.find_by_id(263567475)
 
       expect(item_repository.delete(item_1.id)).to eq(item_1)
       expect(item_repository.find_by_id(item_1.id)).to eq(nil)
